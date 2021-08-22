d35c34a3c2970de0b013681b1bdb2b5cb6cab05e
piotrparobczy <666thenumberofhell>
2021-08-23 00:23:17 +0200

    initialize

---
+++ src/main/java/com/apator/virtualQueue/VirtualQueueApplication.java
@@ -0,0 +1,13 @@
+package com.apator.virtualQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apator/virtualQueue/VirtualQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apator.virtualQueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
